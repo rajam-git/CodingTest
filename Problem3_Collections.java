8c8406f223c17c2b041505096a9f9fb224926aaf
rajam-git <33766726+rajam-git@users.noreply.github.com>
2018-07-30 19:31:54 -0400

    Update Problem3_Collections.java

--- Problem3_Collections.java
+++ Problem3_Collections.java
@@ -1,6 +1,21 @@
 
 public class Problem3_Collections {
 	public void collectionFun() {
+		Map<Integer,Integer> tempMap = new HashMap<Integer,Integer>();		
+		Set<Integer> tempSet = new HashSet<Integer>();
+		List<Integer> finalList = new ArrayList<Integer>();
+		for(int i=1;i<=100;i++){
+		     tempMap.put(i,i);
+		}
+		tempSet.addAll(tempMap.values());
+		finalList.addAll(tempSet);
+		Collections.sort(finalList);
+		
+		for(Integer finalVal: finalList) {
+			System.out.println(finalVal);
+		}
+		//tempSet.addAll(
+		
 		//Requirements: Generate 100 random numbers between 1-1000.
 		//	Put the numbers into a Map.  Then move them to a Set.
 		//  Then move the numbers into a List.  Then sort the numbers.
