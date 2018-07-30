595de6cd7cdd001e32ea3b088763d82f7cfc8d75
rajam-git <33766726+rajam-git@users.noreply.github.com>
2018-07-30 19:20:37 -0400

    Update Problem2_Recursion.java

--- Problem2_Recursion.java
+++ Problem2_Recursion.java
@@ -1,8 +1,9 @@
 
 public class Problem2_Recursion {
 	public String reverseString(String inputStr) {
+		String retStr = new StringBuffer(inputStr).reverse().toString());
 		//Requirements: reverse the input string by using recursion.
 		
-		return "";
+		return retStr;
 	}
 }
