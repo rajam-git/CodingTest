053ddfddc2231de19a14c4789ba45ae93e3e4cd7
rajam-git <33766726+rajam-git@users.noreply.github.com>
2018-07-30 19:55:58 -0400

    Update Problem4_Match.java

--- Problem4_Match.java
+++ Problem4_Match.java
@@ -4,9 +4,13 @@ import java.util.Set;
 public class Problem4_Match {
 	public boolean doTheyMatch(Problem4_Person p1, Problem4_Person p2) {
 		//Are they equal?
+		System.out.println(p1.equals(p2));
 		
 		//If I add both to a Set does it enforce uniqueness?
 		Set<Problem4_Person> setPerson = new HashSet<Problem4_Person>();
+		setPerson.add(p1);
+		setPerson.add(p2);
+		System.out.println(setPerson.contains(p2));
 		
 		//If both conditions above are true, then return true, else false;
 		return false;
